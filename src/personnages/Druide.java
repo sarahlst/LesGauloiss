b55e06e8f8eee052f037af1e4d12438c5f3cbe86
saralst <laouissetsarah2021@gmail.com>
2024-11-29 11:33:53 +0100

    modification

--- src/personnages/Druide.java
+++ src/personnages/Druide.java
@@ -1,5 +1,49 @@
 package personnages;
 
 public class Druide {
+	private String nom;
+	private int effetPotionMin;
+	private int effetPotionMax;
 
+
+
+	public Druide(String nom, int effetPotionMin, int effetPotionMax) {
+		this.nom = nom;
+		this.effetPotionMin = effetPotionMin;
+		this.effetPotionMax = effetPotionMax;
+		parler("Bonjour, je suis le druide " + nom + " et ma potion peut aller d'une force " + effetPotionMin + " à "
+				+ effetPotionMax + ".");
+	}
+
+	public void parler(String texte) {
+		System.out.println(prendreParole() + "« " + texte + "»");
+	}
+
+	private String prendreParole() {
+		return "Le druide " + nom + " : ";
+	}
+
+	public String getNom() {
+		return nom;
+	}
+
+	public void setNom(String nom) { // on a la fonction on appuyant sur source getter setter 
+		this.nom = nom; // et cela parce que les attribut de la classe sont privee et il faut avoir comme 
+	} // une autorisartion 
+
+	public int getEffetPotionMin() {
+		return effetPotionMin;
+	}
+
+	public void setEffetPotionMin(int effetPotionMin) {
+		this.effetPotionMin = effetPotionMin;
+	}
+
+	public int getEffetPotionMax() {
+		return effetPotionMax;
+	}
+
+public void setEffetPotionMax(int effetPotionMax) {
+	this.effetPotionMax = effetPotionMax;
+}
 }
